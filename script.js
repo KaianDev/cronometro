4fb209c4efb9c262b0f66b7fee3651015984e92f
KaianDev <123319433+KaianDev@users.noreply.github.com>
2023-02-28 00:47:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+let iniciar = document.querySelector('.start');
+let pausar = document.querySelector('.pause');
+let parar = document.querySelector('.stop');
+let ass = document.querySelector('p');
+let hora = document.querySelector('.hora');
+let minuto = document.querySelector('.min');
+let segundo = document.querySelector('.sec');
+
+//Váriaveis do cronometro
+let hr = 0;
+let min = 0;
+let sec = 0;
+
+let intervalo = '';
+
+//Variáveis para teste de click
+let pauserClicado = false;
+let iniciarClicado = false;
+let pararClicado = false;
+
+//Definição padrão para deixar o botão oculto de Pause e Stop
+pausar.style.display = 'none'; 
+parar.style.display = 'none';
+
+function start() {
+    iniciarClicado = true;
+    cron();
+    intervalo = setInterval(cron, 1000);
+    testeClick();
+}
+
+function pause() {
+    clearInterval(intervalo);
+    iniciar.innerHTML = 'Reiniciar'
+    pauserClicado = true;
+    console.log(pauserClicado);
+    testeClick();
+}
+
+function stopT() {
+    clearInterval(intervalo);
+    hora.innerHTML = '00';
+    minuto.innerHTML = '00';
+    segundo.innerHTML = '00';
+    sec = 0;
+    min = 0;
+    hr = 0;
+    iniciar.innerHTML = 'Iniciar';
+    pararClicado = true;
+    testeClick();
+}
+
+function doisDigitos(digito) {
+    if (digito < 10) {
+        return '0' + digito;
+    } else {
+        return digito;
+    }
+}
+function cron() {
+    sec++;
+    segundo.innerHTML = doisDigitos(sec);
+    if (sec == 60) {
+        sec = 0;
+        min++;
+        minuto.innerHTML = doisDigitos(min);
+        if (min == 60) {
+            min = 0;
+            hr++
+            hora.innerHTML = doisDigitos(hr);
+        }
+    }
+}
+
+function testeClick() {
+    if (iniciarClicado == true) {
+        iniciar.style.display = 'none';
+        pausar.style.display = 'inline-block'
+        parar.style.display = 'none';
+        iniciarClicado = false;
+    }
+    if (pauserClicado == true) {
+        iniciar.style.display = 'inline-block';
+        pausar.style.display = 'none';
+        parar.style.display = 'inline-block';
+        pauserClicado = false;
+    }
+    if (pararClicado == true) {
+        iniciar.style.display = 'inline-block';
+        pausar.style.display = 'none';
+        parar.style.display = 'none';
+        pararClicado = false;
+    }
+}
+
+
+ass.innerHTML += ' - ' + new Date().getFullYear()
+
+iniciar.addEventListener('click', start);
+pausar.addEventListener('click', pause);
+parar.addEventListener('click', stopT);
